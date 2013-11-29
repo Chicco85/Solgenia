ec227baeeb453d11d0a45652a01c659017de1e98
t-wmariani <t-wmariani@CLT-SPO59.SOLGENIA.NET>
2013-11-29 10:23:18 +0100

    29/11/2013

--- SecureTest/src/com/solgenia/secure/test/ws/ClientTestPrime.java
+++ SecureTest/src/com/solgenia/secure/test/ws/ClientTestPrime.java
@@ -9,8 +9,24 @@ import javax.xml.ws.Service;
 
 public class ClientTestPrime {
 
+   static {
+      //for localhost testing only
+      javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+      new javax.net.ssl.HostnameVerifier(){
+
+          @Override
+         public boolean verify(String hostname,
+                  javax.net.ssl.SSLSession sslSession) {
+              if (hostname.equals("localhost")) {
+                  return true;
+              }
+              return false;
+          }
+      });
+  }
+
    public static void main(String[] args) throws MalformedURLException {
-      URL url=new URL("http://localhost:8080/SecureTest/PrimeNumber?wsdl");
+      URL url=new URL("https://localhost:8443/SecureTest/PrimeNumber?wsdl");
       QName qName=new QName("http://ws.test.secure.solgenia.com", "PrimeNumber");
       int number;
       Service service=Service.create(url, qName);
